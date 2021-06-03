bd90825cd745fd20315715506e51b3aab90dbad3
N8Brown <nathan.a.brown@outlook.com>
2021-06-03 15:38:40 -0400

    update code to reflect course progress through unit 18

--- django_flashcards/flashcards/urls.py
+++ django_flashcards/flashcards/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('subtraction/', views.subtraction, name='subtraction'),
+    path('addition/', views.addition, name='addition'),
+    path('multiplication/', views.multiplication, name='multiplication'),
+    path('division/', views.division, name='division'),
 ]
--- django_flashcards/flashcards/views.py
+++ django_flashcards/flashcards/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def addition(request):
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        context = {
+            'answer':answer, 
+        }
+        return render(request, 'addition.html', context)
+    
+    return render(request, 'addition.html')
+
+def subtraction(request):
+    return render(request, 'subtraction.html')
+    
+def multiplication(request):
+    return render(request, 'multiplication.html')
+    
+def division(request):
+    return render(request, 'division.html')
