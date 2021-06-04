bcc52fe05ea8e8c9385ead682b534fc1da352932
N8Brown <nathan.a.brown@outlook.com>
2021-06-04 14:12:53 -0400

    update code to reflect course progress through unit 25

--- django_flashcards/flashcards/views.py
+++ django_flashcards/flashcards/views.py
@@ -5,14 +5,28 @@ def home(request):
     return render(request, 'home.html')
 
 def addition(request):
+    from random import randint
+    num1 = randint(0,10)
+    num2 = randint(0,10)
+
     if request.method == 'POST':
-        answer = request.POST['answer']
+        question_num1 = int(request.POST['question_num1'])
+        question_num2 = int(request.POST['question_num2'])
+        answer = (request.POST['answer'])
+        correct_answer = question_num1 + question_num2
+
         context = {
-            'answer':answer, 
-        }
+            'answer':answer,
+            'correct_answer': correct_answer,
+            }
         return render(request, 'addition.html', context)
-    
-    return render(request, 'addition.html')
+
+    context = {
+        'num1': num1,
+        'num2': num2, 
+    }
+
+    return render(request, 'addition.html', context)
 
 def subtraction(request):
     return render(request, 'subtraction.html')
